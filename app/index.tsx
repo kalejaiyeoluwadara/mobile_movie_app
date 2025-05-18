ebb0e86f421031080e54cd1a93186ea2152cf7cd
kalejaiyeoluwadara <kalejaiyeoluwadara1@gmail.com>
2025-05-18 23:39:20 +0100

    Enhance styling and configuration
    - Added `react-native-reanimated/plugin` to Babel configuration for improved animation support.
    - Updated Tailwind CSS configuration to include additional color definitions and adjusted content paths for better component coverage.
    - Changed text color in the main index file to enhance visibility.

--- app/index.tsx
+++ app/index.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View className="flex-1 items-center justify-center bg-red-500">
-      <Text className="text-black">Hello World</Text>
+      <Text className="text-green-200">Hello</Text>
     </View>
   );
 }
