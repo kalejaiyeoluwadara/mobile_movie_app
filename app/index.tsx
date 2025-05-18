5458543370d14463d2ab9c4de9b8596ca3383957
kalejaiyeoluwadara <kalejaiyeoluwadara1@gmail.com>
2025-05-18 23:29:41 +0100

    Update dependencies and refactor project structure
    - Added `nativewind` and `tailwindcss` to dependencies in `package.json` and `package-lock.json`.
    - Updated version constraints for `react-native-safe-area-context`.
    - Removed unused components and files, including various screens and UI components.
    - Simplified the root layout by removing unnecessary imports and logic.
    - Added `nativewind-env.d.ts` to TypeScript configuration for type support.
    - Cleaned up project structure by deleting obsolete files and directories.

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center bg-red-500">
+      <Text className="text-black">Hello World</Text>
+    </View>
+  );
+}
